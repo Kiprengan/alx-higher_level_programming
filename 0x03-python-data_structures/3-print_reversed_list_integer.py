9ab90fc1920f7465f6791876a25b94aa66b419ff
Denis Kiprono <deniskiprono881@gmail.com>
2023-06-12 02:04:40 -0700

    Reverse the items in list when printing

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
-    new_list=my_list.reverse()
-    for m in new_list:
-        print("{:d}".format(new_list[m])
+    my_list.reverse()
+    for m in range(len(my_list)):
+        print("{:d}".format(my_list[m])
