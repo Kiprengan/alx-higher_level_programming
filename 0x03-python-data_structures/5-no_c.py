d2f484346ed06ed2683262341a553969dd8a5142
Denis Kiprono <deniskiprono881@gmail.com>
2023-06-12 11:45:14 -0700

    Removes c or C

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def no_c(my_string):
+    new_string = ''
+    for m in my_string:
+        if m != 'c' and m != 'C':
+            new_string = new_string + m
+    return new_string
