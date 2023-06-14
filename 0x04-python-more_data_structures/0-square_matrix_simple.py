1990dea49ac63501d5faf85fa515721dda7ec740
Denis Kiprono <deniskiprono881@gmail.com>
2023-06-14 09:05:26 -0700

    Fixed

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -2,5 +2,5 @@
 def square_matrix_simple(matrix=[]):
     new_mtrix = []
     for m in matrix:
-        new.append([k ** 2 for k in m])
+        new_matrix.append([k ** 2 for k in m])
     return new_matrix
