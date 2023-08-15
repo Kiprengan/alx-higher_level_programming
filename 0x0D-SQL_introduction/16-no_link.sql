3755fe3d01d194c18f648d6e27ea8a9420261a9e
Denis Kiprono <deniskiprono881@gmail.com>
2023-08-15 06:11:39 -0700

    More on SQL

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,3 +1,5 @@
 -- Ommits records withoyt value in name column
-SELECT score, name FROM second_table WHERE name NOT NULL
+SELECT score, name
+FROM second_table
+WHERE name IS NOT NULL
 ORDER BY score DESC;
--- 0x0D-SQL_introduction/8-count_89.sql
+++ 0x0D-SQL_introduction/8-count_89.sql
@@ -1,2 +1,2 @@
 -- List number of records having id =89
-SELECT COUNT(*) FROM first_table where `id` = 89;
+SELECT COUNT(*) FROM first_table WHERE `id` = 89;
